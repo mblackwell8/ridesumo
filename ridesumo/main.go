d6b6ca5334058f95364116e5ef0132ebfd5389bd
Mark Blackwell <mblackwell8@gmail.com>
2015-07-12 20:02:44 +1000

    working

--- ridesumo/main.go
+++ ridesumo/main.go
@@ -2,20 +2,41 @@ package ridesumo
 
 import (
 	// "fmt"
+	"appengine"
+	"appengine/urlfetch"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
-	// "github.com/gorilla/sessions"
+	"github.com/gorilla/sessions"
+	"github.com/strava/go.strava"
+	"github.com/unrolled/render"
+	"log"
+	"middleware"
 	"net/http"
-	"sessionauth"
 )
 
+var renderer = render.New(render.Options{
+	IndentJSON: true,
+})
+
+// default sessions last for one month
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
+var sessionName = "rs_sess"
+
 func init() {
-	app := negroni.Classic()
+	app := negroni.New(
+		negroni.NewRecovery(), 
+		negroni.NewLogger(), 
+		negroni.NewStatic(http.Dir("static")))
 
-	// store := sessions.NewCookieStore([]byte("something-very-secret"))
-	// app.Use(sessionauth.SessionUser(store, "rs_sess", GenerateAnonymousUser))
+	log.Println("setting AE middleware")
+	// must be done before session because it needs the appengine context
+	app.Use(negroni.HandlerFunc(middleware.CreateAppEngineContext))
 
+	log.Println("setting session middleware")
+	app.Use(middleware.SessionUser(store, sessionName, GenerateAnonymousUser))
+
+	initStravaOAuth()
 	app.UseHandler(makeRoutes())
 
 	http.Handle("/", context.ClearHandler(app))
@@ -23,18 +44,55 @@ func init() {
 
 func makeRoutes() http.Handler {
 	router := mux.NewRouter()
-	// router.HandleFunc("/", TempRedirect)
+	router.HandleFunc("/", Home).Methods("GET")
 	router.HandleFunc("/test", Test).Methods("GET")
-	router.HandleFunc("/login", Login).Methods("GET")
+	router.HandleFunc("/register", Register).Methods("POST")
+
+	path, _ := stravaOAuth.CallbackPath()
+	router.HandleFunc(path, stravaOAuth.HandlerFunc(oAuthSuccess, oAuthFailure))
 
 	userRoutes := mux.NewRouter()
-	userRoutes.HandleFunc("/test", Test).Methods("GET")
+	userRoutes.HandleFunc("/u/dashboard", Dashboard).Methods("GET")
+	userRoutes.HandleFunc("/u/comps", GetComps).Methods("GET")
+	userRoutes.HandleFunc("/u/comps/{id}", GetComp).Methods("GET")
+	userRoutes.HandleFunc("/u/comps", CreateComp).Methods("POST")
+	userRoutes.HandleFunc("/u/comps/{id}", EditComp).Methods("POST")
 
-	// Create a new negroni for the user middleware
-	router.Handle("/user", negroni.New(
-		negroni.HandlerFunc(sessionauth.LoginRequired),
+	router.PathPrefix("/u").Handler(negroni.New(
+		negroni.HandlerFunc(middleware.LoginRequired),
 		negroni.Wrap(userRoutes),
 	))
 
+	// router.HandleFunc("/login", GetLogin).Methods("GET")
+	// router.HandleFunc("/login", Login).Methods("POST")
+
+	// userRoutes := router.Path("/u").Subrouter()
+	// router.HandleFunc("/u/test", Test).Methods("GET")
+	// userRoutes.HandleFunc("/dashboard", Test).Methods("GET")
+
+	// Create a new negroni for the user middleware
+	// router.Handle("/u", negroni.New(
+	// 	negroni.HandlerFunc(sessionauth.LoginRequired),
+	// 	negroni.Wrap(userRoutes),
+	// ))
+
 	return router
+}
+
+var stravaOAuth *strava.OAuthAuthenticator
+
+func initStravaOAuth() {
+	strava.ClientId = 6897
+	strava.ClientSecret = "b63e66cfc9f1280d0ad5fa3d7eeeb40e10628cba"
+	stravaOAuth = &strava.OAuthAuthenticator{
+		CallbackURL: "http://localhost:8080/exchange_token",
+		RequestClientGenerator: func(r *http.Request) *http.Client {
+			return urlfetch.Client(appengine.NewContext(r))
+		},
+	}
+}
+
+func Test(w http.ResponseWriter, r *http.Request) {
+	renderer.HTML(w, http.StatusOK, "default", nil)
+	// renderDefaultHTML(w, "default", nil)
 }
--- ridesumo/user.go
+++ ridesumo/user.go
@@ -1,21 +1,96 @@
 package ridesumo
 
 import (
-	"sessionauth"
+	"appengine/datastore"
+	"log"
+	"middleware"
+)
+
+const (
+	DatastoreNameUser string = "user"
 )
 
 type User struct {
-	uniqueId string
-	Name     string
-	Email    string
+	Email string
+
+	FirstName, LastName  string
+	City, State, Country string
+	Gender               string
+	StravaAccessToken    string
+
+	// should probably store last update on this
+	// HACK: VERY tight coupling here... what if Strava goes away?
+	// datastore can't take ClubSummary type for some reason... maybe because it's an array?
+	// StravaUserRecord strava.AthleteDetailed
+
+	// Strava does it like this
+	// "access_token":"a9cf6599f14fb7a81eb7e9731b60909acc0bc671",
+	// "token_type":"Bearer",
+	// "athlete":{
+	// 	"id":323132,
+	// 	"resource_state":3,
+	// 	"firstname":"Mark",
+	// 	"lastname":"Blackwell",
+	// 	"profile_medium":"https://dgalywyr863hv.cloudfront.net/pictures/athletes/323132/2690845/1/medium.jpg",
+	// 	"profile":"https://dgalywyr863hv.cloudfront.net/pictures/athletes/323132/2690845/1/large.jpg",
+	// 	"city":"Sydney",
+	// 	"state":"New South Wales",
+	// 	"country":"Australia",
+	// 	"sex":"M",
+	// 	"friend":null,
+	// 	"follower":null,
+	// 	"premium":true,
+	// 	"created_at":"2012-03-14T04:59:12Z",
+	// 	"updated_at":"2015-06-22T22:16:45Z",
+	// 	"badge_type_id":1,
+	// 	"follower_count":32,
+	// 	"friend_count":36,
+	// 	"mutual_friend_count":0,
+	// 	"athlete_type":0,
+	// 	"date_preference":"%d/%m/%Y",
+	// 	"measurement_preference":"meters",
+	// 	"email":"mblackwell8@gmail.com",
+	// 	"ftp":310,
+	// 	"weight":74.5,
+	// 	"clubs":[],
+	// 	"bikes":[],
+	// 	"shoes":[]
+	// }
+
+	// Only exported fields (beginning with an upper case letter) will be saved to the datastore
+	// uniqueId      int64
+	// an internal cache of the datastore.Key
+	datastoreKey *datastore.Key
 
 	authenticated bool
 }
 
 // GetAnonymousUser should generate an anonymous user model
 // for all sessions. This should be an unauthenticated 0 value struct.
-func GenerateAnonymousUser() sessionauth.User {
+func GenerateAnonymousUser() middleware.User {
 	return &User{}
+}
+
+func NewUser(email, firstName, lastName string) *User {
+	newUser := &User{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+	}
+
+	return newUser
+}
+
+func (u *User) Put() error {
+	var err error
+	if u.datastoreKey == nil {
+		newKey := datastore.NewIncompleteKey(middleware.AppEngineContext, DatastoreNameUser, nil)
+		u.datastoreKey, err = datastore.Put(middleware.AppEngineContext, newKey, u)
+	} else {
+		_, err = datastore.Put(middleware.AppEngineContext, u.datastoreKey, u)
+	}
+
+	return err
 }
 
 // Login will preform any actions that are required to make a user model
@@ -40,29 +115,19 @@ func (u *User) IsAuthenticated() bool {
 }
 
 func (u *User) UniqueId() interface{} {
-	return u.uniqueId
+	return u.datastoreKey.IntID()
 }
 
 // GetById will populate a user object from a database model with
 // a matching id.
 // func (u *User) GetById(id interface{}) error {
 func (u *User) GetById(id interface{}) error {
-	// log.Println("GetById called for user: " + id.(string))
-	// may want to speed this up by storing the user in the datastore and only refreshing on command
-	// context := appengine.NewContext(req)
-	// var err error
-	// u.data, err = nxtcutils.GetUser(context, id.(string), "shippingAddresses", "paymentCards", "orders", "scripts", "scripts.product")
-	// if err != nil {
-	// 	return err
-	// }
+	log.Printf("looking up user ID %d", id)
+	u.datastoreKey = datastore.NewKey(middleware.AppEngineContext, DatastoreNameUser, "", id.(int64), nil)
+	err := datastore.Get(middleware.AppEngineContext, u.datastoreKey, u)
+	if err != nil {
+		log.Panicf("ERROR looking up user ID %s", err.Error())
+	}
 
-	// u.Username = u.data["username"].(string)
-	// u.UniqId = u.data["objectId"].(string)
-
-	// err := dbmap.SelectOne(u, "SELECT * FROM users WHERE id = $1", id)
-	// if err != nil {
-	// 	return err
-	// }
-
-	return nil
+	return err
 }
